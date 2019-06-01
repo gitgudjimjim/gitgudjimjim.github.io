55116b2963bfa2e86df9f66f4deb6d8a1002dec0
Jim <jhyl1987@icloud.com>
2019-05-31 21:47:29 -0700

    second try commit
    Oregon vet site

---
+++ Jim_Capstone/JimCapstone/capapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# # Register your models here.
+# admin.site.register(Model)
+#
+# from django.contrib import admin
+# from .models import <model name 1> <model name 2>
+# admin.site.register(<model name 1>)
+# admin.site.register(<model name 2>)
