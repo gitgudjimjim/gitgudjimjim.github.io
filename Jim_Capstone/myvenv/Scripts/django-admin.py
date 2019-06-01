8027a0870f8e4e37d9a3ed556c0cc71a11582979
Jim <jhyl1987@icloud.com>
2019-05-31 22:15:23 -0700

    Revert "3rd try"
    This reverts commit 28dc6e5e62ce5f2268396be4a883613c0ce7bd59.

--- Jim_Capstone/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jhyl1\documents\django\jim_capstone\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ README.md
@@ -0,0 +1,1 @@
+# gitgudjimjim.github.io
